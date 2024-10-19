8a889967f84bdcfd7c10e330af8fb9b09ef5d97e
Koketso Choeu <chueukeutlwilekoketso@gmail.com>
2024-10-19 18:54:34 +0200

    Website Structure

--- utils.ts
+++ utils.ts
@@ -3,7 +3,7 @@ const returningUserDisplay = document.querySelector('#returning-user')
 const userNameDisplay = document.querySelector('#user')
 
 import { LoyaltyUser, Permissions } from './enums'
-import { Review } from './interfaces'
+import  Review  from './interfaces'
 
 
 export function showReviewTotal(value: number, reviewer: string, isLoyalty: LoyaltyUser) {
