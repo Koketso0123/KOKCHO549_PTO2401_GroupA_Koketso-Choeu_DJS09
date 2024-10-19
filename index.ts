631006bfd8808878a27dbcf09cfcf4e5fc6cb85f
Koketso Choeu <chueukeutlwilekoketso@gmail.com>
2024-10-19 16:07:24 +0200

    Enum Types

--- index.ts
+++ index.ts
@@ -44,18 +44,41 @@ function showReviewTotal (value : number, reviewer: string, isLoyalty : boolean)
 
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
 
-const you: {
-    firstName: string;
-    lastName: string;
-    isReturning: boolean;
-    age: number;
-    stayedAt: string[];
-} = {
+// User
+// const you: {
+//     firstName: string;
+//     lastName: string;
+//     isReturning: boolean;
+//     age: number;
+//     stayedAt: string[]
+// } = {
+//     firstName: 'Bobby',
+//     lastName: 'Brown',
+//     isReturning: true,
+//     age: 35,
+//     stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
+// }
+
+const ADMIN = 'admin'
+const READ_ONLY = 'read-only'
+
+
+enum Permissions {
+    ADMIN,
+    READ_ONLY
+}
+
+const you = {
     firstName: 'Bobby',
     lastName: 'Brown',
+    permissions: Permissions.ADMIN,
     isReturning: true,
     age: 35,
     stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
+}
+
+if (you.permissions === 'admin') {
+    //show
 }
 
 const properties : {
